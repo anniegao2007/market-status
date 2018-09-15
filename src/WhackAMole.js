4615dace926bf9df9a3169ee6cb6826f01997454
Annie Gao <anniegao2007@gmail.com>
2018-09-15 17:20:33 -0400

    made grid for whackamole

--- src/WhackAMole.js
+++ src/WhackAMole.js
@@ -20,18 +20,24 @@ export class WhackAMole extends React.Component {
             <div>
                 <h1>Whack-A-Mole!</h1>
                 <h3>A more amusing visualization of stock trade timing...</h3>
-                <Hole className="WAMhole" id={0} mole={this.state.moleHole == 0}/>
-                <Hole className="WAMhole" id={1} mole={this.state.moleHole == 1}/>
-                <Hole className="WAMhole" id={2} mole={this.state.moleHole == 2}/>
-                <br />
-                <Hole className="WAMhole" id={3} mole={this.state.moleHole == 3}/>
-                <Hole className="WAMhole" id={4} mole={this.state.moleHole == 4}/>
-                <Hole className="WAMhole" id={5} mole={this.state.moleHole == 5}/>
-                <br />
-                <Hole className="WAMhole" id={6} mole={this.state.moleHole == 6}/>
-                <Hole className="WAMhole" id={7} mole={this.state.moleHole == 7}/>
-                <Hole className="WAMhole" id={8} mole={this.state.moleHole == 8}/>
-                <br />
+                <div className="holesRow">
+                    <Hole id={0} mole={this.state.moleHole == 0}/>
+                    <Hole id={1} mole={this.state.moleHole == 1}/>
+                    <Hole id={2} mole={this.state.moleHole == 2}/>
+                    <br />
+                </div>
+                <div className="holesRow">
+                    <Hole id={3} mole={this.state.moleHole == 3}/>
+                    <Hole id={4} mole={this.state.moleHole == 4}/>
+                    <Hole id={5} mole={this.state.moleHole == 5}/>
+                    <br />
+                </div>
+                <div className="holesRow">
+                    <Hole id={6} mole={this.state.moleHole == 6}/>
+                    <Hole id={7} mole={this.state.moleHole == 7}/>
+                    <Hole id={8} mole={this.state.moleHole == 8}/>
+                    <br />
+                </div>
                 <h2>Total Score: {this.state.score}</h2>
             </div>
         )
