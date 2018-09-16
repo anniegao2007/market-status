fd4c0982dd2229e9b002063b2700e679e8b2d85c
Annie Gao <anniegao2007@gmail.com>
2018-09-15 20:44:56 -0400

    made whackamole more evil >:)

--- src/WhackAMole.js
+++ src/WhackAMole.js
@@ -6,6 +6,7 @@ export class WhackAMole extends React.Component {
         super(props);
         this.state = {
             score: 0,
+            hits: 0,
             misses: 0,
             moleHole: null,
             message: null,
@@ -21,13 +22,14 @@ export class WhackAMole extends React.Component {
     onClickCircle(c) {
         let message = "";
         if(c === this.state.moleHole) {
+            let delta = Math.floor(Math.random() * 10) - 3;
             if(this.state.score === 9) {
                 clearInterval(this.interval);
                 message = "You Win!";
             } else {
-                message = "Hit!";
+                message = `Hit! Your score just increased by ${delta}!`;
             }
-            this.setState({ moleHole: -1, score: this.state.score+1, message });
+            this.setState({ moleHole: -1, hits: this.state.hits+1, score: this.state.score+delta, message });
         } else {
             message = "Missed!";
             this.setState({ message, misses: this.state.misses+1 });
@@ -44,12 +46,18 @@ export class WhackAMole extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1 className="centertext">Whack-A-Mole!</h1>
-                <h3 className="centertext">A more amusing visualization of stock trade timing...</h3>
+            <div className="centertext">
+                <h1>Whack-A-Mole!</h1>
+                <h3>A more amusing visualization of stock trade timing...</h3>
+                <p>
+                    The rules are simple. The moles are fast. You will miss many of them. Just like you will probably miss many prime stocktrading opportunities.
+                    Each opportunity you do seize is a risk, however. You do not know how much you will win or lose.
+                    This game ends when you reach 20 points.
+                    Good luck, grasshopper.
+                </p>
                 <br />
                 <button onClick={this.stop}>I gIvE UP</button>
-                <h4 className="centertext">{this.state.message}</h4>
+                <h4>{this.state.message}</h4>
                 <div className="holesRow">
                     <Hole id={0} mole={this.state.moleHole === 0} onClick={() => this.onClickCircle(0)}/>
                     <Hole id={1} mole={this.state.moleHole === 1} onClick={() => this.onClickCircle(1)}/>
@@ -68,9 +76,10 @@ export class WhackAMole extends React.Component {
                     <Hole id={8} mole={this.state.moleHole === 8} onClick={() => this.onClickCircle(8)}/>
                     <br />
                 </div>
-                <h2 className="centertext">Total Score: {this.state.score}</h2>
-                <h2 className="centertext">Total Misses: {this.state.misses}</h2>
-                <h2 className="centertext">Accuracy: {this.state.score===0 ? 0 : (this.state.score / (this.state.score + this.state.misses)).toFixed(3)}</h2>
+                <h2>Total Score: {this.state.score}</h2>
+                <h2>Total Hits: {this.state.hits}</h2>
+                <h2>Total Misses: {this.state.misses}</h2>
+                <h2>Accuracy: {this.state.hits===0 ? 0 : (this.state.hits / (this.state.score + this.state.misses)).toFixed(3)}</h2>
             </div>
         )
     }
